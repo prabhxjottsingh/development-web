faa1396e8b7abd761dfee03c60a77e06d369644a
Prabhxjott <prabhx.jott@gmail.com>
2022-01-02 01:23:13 +0530

    Custom Backend - expressJS(alt)

---
+++ nodeJS/06-customBackend/code.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const http = require("http");
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((re1, res)=>{
+    res.statusCode = 200;
+    console.log(req.url);
+    res.setHeader('content-type', 'text-plain');
+    res.end('Hello, World');
+})
+
+server.listen(port, hostname, ()=>{
+    console.log("Server Running at http:// ${hostname}: ${port}");
+})
--- nodeJS/blockingAndNonBlocking.js
+++ nodeJS/blockingAndNonBlocking.js
@@ -3,3 +3,11 @@
 
 //Asynchronous OR Non-Blocking Code -> Line by line execution is not guranteed
 //-CallBacks will fire
+
+const fs = require("fs");
+let text = fs.readFileSync("Development.txt", "utf-8", (err,data)=>{
+    console.log(data);
+});
+console.log("This is a message");
+
+console.log(text);
